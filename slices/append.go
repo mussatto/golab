fbda3af2d04a81e237510061e6425146a568d8d4
Vitor Mussatto <mussatto@gmail.com>
2016-11-09 15:42:42 +0100

    concat, function

--- slices/append.go
+++ slices/append.go
@@ -3,14 +3,10 @@ package slices
 import "fmt"
 
 func DoAppendSlices(){
-
 	myStringSlice := []string{"first", "second", "third"}
-
 	//ERROR!
 	//myStringSlice = append(myStringSlice, []string{"fourth", "fift"})
-
 	myStringSlice = append(myStringSlice, []string{"fourth", "fift"}...)
 	myStringSlice = append(myStringSlice, "sixth", "seventh")
 	fmt.Println(myStringSlice)
-
 }
